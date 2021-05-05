2af956a62b3e39251328828748747a7d7aa2d529
Marco <fracassi.marco@gmail.com>
2021-05-05 09:54:15 +0200

    Add Player object

---
+++ src/main/java/kata/Player.java
@@ -0,0 +1,36 @@
+package kata;
+
+import java.util.List;
+
+public class Player {
+    private final String name;
+    private int score = 0;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public boolean is(String name) {
+        return this.name.equals(name);
+    }
+
+    public void addPoint() {
+        score++;
+    }
+
+    public boolean hasSameScore(Player other) {
+        return other.score == score;
+    }
+
+    public boolean isCloseToWin() {
+        return score >= 4;
+    }
+
+    public String printScore(List<String> scores) {
+        return scores.get(score);
+    }
+
+    public int scoreGap(Player other) {
+        return score - other.score;
+    }
+}
--- src/main/java/kata/TennisGame.java
+++ src/main/java/kata/TennisGame.java
@@ -1,28 +1,28 @@
 package kata;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
 
 public class TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private final Player player1;
+    private final Player player2;
 
     public TennisGame(String player1Name, String player2Name) {
+        player1 = new Player(player1Name);
+        player2 = new Player(player2Name);
     }
 
     public void wonPoint(String playerName) {
-        if (playerName.equals("player1"))
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        Stream.of(player1, player2).filter(p -> p.is(playerName)).findFirst().get().addPoint();
     }
 
     public String getScore() {
-        if (m_score1 == m_score2) {
+        if (player1.hasSameScore(player2)) {
             return deuce();
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
+        } else if (player1.isCloseToWin() || player2.isCloseToWin()) {
             return closeToWin();
         } else {
             return inTheGame();
@@ -31,11 +31,11 @@ public class TennisGame {
 
     private String inTheGame() {
         List<String> scores = asList("Love", "Fifteen", "Thirty", "Forty");
-        return scores.get(m_score1) + "-" + scores.get(m_score2);
+        return player1.printScore(scores) + "-" + player2.printScore(scores);
     }
 
     private String closeToWin() {
-        int minusResult = m_score1 - m_score2;
+        int minusResult = player1.scoreGap(player2);
         if (minusResult == 1) return "Advantage player1";
         if (minusResult == -1) return "Advantage player2";
         if (minusResult >= 2) return "Win for player1";
@@ -43,6 +43,6 @@ public class TennisGame {
     }
 
     private String deuce() {
-        return asList("Love-All", "Fifteen-All", "Thirty-All", "Deuce", "Deuce").get(m_score1);
+        return player1.printScore(asList("Love-All", "Fifteen-All", "Thirty-All", "Deuce", "Deuce"));
     }
 }
