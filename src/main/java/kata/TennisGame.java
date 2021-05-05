856c2c7af209400ac87f4075a4fe18c73bffa661
Marco <fracassi.marco@gmail.com>
2021-05-05 09:24:13 +0200

    Extract closeToWin

--- src/main/java/kata/TennisGame.java
+++ src/main/java/kata/TennisGame.java
@@ -25,11 +25,7 @@ public class TennisGame {
         if (m_score1 == m_score2) {
             return deuce();
         } else if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
+            score = closeToWin();
         } else {
             for (int i = 1; i < 3; i++) {
                 if (i == 1) tempScore = m_score1;
@@ -54,6 +50,14 @@ public class TennisGame {
             }
         }
         return score;
+    }
+
+    private String closeToWin() {
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) return "Advantage player1";
+        if (minusResult == -1) return "Advantage player2";
+        if (minusResult >= 2) return "Win for player1";
+        return "Win for player2";
     }
 
     private String deuce() {
