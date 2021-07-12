2df7c3dc2ca02ae9005c19dbcb94eff263a41da6
Marco <fracassi.marco@gmail.com>
2021-07-12 09:48:01 +0200

    Cleanup

--- src/main/java/kata/TennisGame.java
+++ src/main/java/kata/TennisGame.java
@@ -1,11 +1,14 @@
 package kata;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TennisGame {
-    
+
     private int m_score1 = 0;
     private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -13,65 +16,34 @@ public class TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals(player1Name))
             m_score1 += 1;
         else
             m_score2 += 1;
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        if (m_score1 == m_score2) {
+            Map<Integer, String> scores = new HashMap<>();
+            scores.put(0, "Love-All");
+            scores.put(1, "Fifteen-All");
+            scores.put(2, "Thirty-All");
+            scores.put(3, "Deuce");
+            scores.put(4, "Deuce");
+            return scores.get(m_score1);
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            int minusResult = m_score1 - m_score2;
+            if (minusResult == 1) return "Advantage " + player1Name;
+            if (minusResult == -1) return "Advantage " + player2Name;
+            if (minusResult >= 2) return "Win for " + player1Name;
+            return "Win for " + player2Name;
+        } else {
+            Map<Integer, String> scores = new HashMap<>();
+            scores.put(0, "Love");
+            scores.put(1, "Fifteen");
+            scores.put(2, "Thirty");
+            scores.put(3, "Forty");
+            return scores.get(m_score1) + "-" + scores.get(m_score2);
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
-        return score;
     }
 }
